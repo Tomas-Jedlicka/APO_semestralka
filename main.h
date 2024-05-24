56c40a9c0e5739360a6bed1b6fcfc144a0bbd705
Tomas Jedlicka <jedlito8@fel.cvut.cz>
2024-05-24 14:47:51 +0200

    Added description and compilation details

--- main.h
+++ main.h
@@ -1,3 +1,29 @@
+/***********************************************************************************************
+ * This is the source code of a game based on movie from 1984 named Tron.
+ * ============================================================================================= 
+ * 
+ * Hardware:
+ *  - MICROZED EVALUATION KIT
+ *  - CPU -> Dual ARM® Cortex™-A9 MPCore™ @ 866 MHz
+ *  - RAM -> 2x L1 32 kB data +32 kB instruction, L2 512 KB
+ *  - Operation system -> GNU/Linux
+ *      - GNU LIBC (libc6) 2.28-10
+ *      - Jádro Linux 4.19.59-rt22-00005-gedf9096397ae src
+ *      - Distribuce: Debian 10 Buster
+ * 
+ * More information about hardware:
+ * -> https://www.avnet.com/wps/portal/us/products/avnet-boards/avnet-board-families/microzed/
+ * -> https://gitlab.com/pikron/projects/mz_apo/microzed_apo 
+ * 
+ * Compilation:
+ *  - gcc (arm-linux-gnueabihf-gcc)
+ *  - binary package needed (Debian/ubuntu => "crossbuild-essential-armhf")
+ *  - u need to build it first with make file
+ *  - only need to change IP in the make file with an adress of the board
+ *  - than run the binary with command "make run" on the board's terminal window
+ * 
+*************************************************************************************************/
+
 #ifndef MAIN_H
 #define MAIN_H
 
